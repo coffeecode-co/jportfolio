e73116ace231f95968e3976233e40a6653cc28a6
juanleon8581 <juanleon8581@gmail.com>
2026-05-15 02:07:06 -0500

    feat: add Docucofi project page and update project details component to support GitHub links

--- src/components/MyProjects.tsx
+++ src/components/MyProjects.tsx
@@ -29,6 +29,14 @@ export const MyProjects = () => {
       seeMoreUrl: t('mangoAssistant.seeMore'),
     },
     {
+      projectName: 'Docucofi',
+      imgUrl:
+        'https://pub-fda94ed7b1d0487db34447feefb77dbb.r2.dev/docucofi.png',
+      alt: 'Docucofi - Document Generator',
+      url: 'https://docucofi.coficode.dev/',
+      seeMoreUrl: t('docucofi.seeMore'),
+    },
+    {
       projectName: 'Espíritu Guerrero',
       imgUrl:
         'https://pub-fda94ed7b1d0487db34447feefb77dbb.r2.dev/espiritu-thumbnail-4-3.png',
